ed215b596b2df56eb86dd9f94109c8806b9728b5
himanshu8686 <him.sharma8686@gmail.com>
2020-04-05 23:38:07 +0530

    controlled component project form is completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AddProject from './components/projects/AddProject';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <a href="/projectForm"> create a project</a>
+      <Router>
+
+        <Route path="/projectForm" component={AddProject} />
+      </Router>
     </div>
   );
 }
